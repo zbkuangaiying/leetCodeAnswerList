495c8f4d88dfc0a65ea487e06a86459eaed7df89
zbkuangaiying <zbkuangaiying@163.com>
2019-05-21 09:36:38 +0800

    No.172 FactorialTrailingZeroes done

---
+++ src/easy/FactorialTrailingZeroes.java
@@ -0,0 +1,42 @@
+package easy;
+
+import java.math.BigInteger;
+
+/**
+ * @author GK
+ * @version 创建时间：2019-5-21 上午9:15:24
+ * @ClassName 类名称
+ * @Description 类描述
+ * 
+ * 给定一个整数 n，返回 n! 结果尾数中零的数量。
+ * 5! 1*2*3*4*5 = 2*5*3*4 = 10 * 12 = 120 1个0
+ * 10! 1*2*3*4*5*6*7*8*9*10 = (2*5) * 3*4 * 6*7*8*9*(1*10) = 12*42*72*10*10 = 2*2*2 尾数一定是8 而不会是0
+ * 因此 只需要查询在阶乘中每个能被5整除的个数就OK
+ * 10! 10/5 = 2
+ * 15! 15/5 = 3
+ * 20! 20/5 = 4
+ * 25! 25/5 = 5  ???? 这里还有一个....还能再乘以一次5   需要继续除下去 5/5 = 1   因此  25! = 25/5+5/5 = 6 125同理
+ * 
+ * 
+ */
+public class FactorialTrailingZeroes extends LeetcodeEasyLevel {
+	
+	public static void main(String[] args) {
+		long l = trailingZeroes(6);
+		System.out.println(l);
+	}
+
+	 public static  int trailingZeroes(int n) {
+	        if(n < 5){
+	        	return 0;
+	        }
+	        int count = 0;
+	        while(n >= 5){
+	        	count += n/5;
+	        	n = n / 5;
+	        }
+	        return count;
+	        
+	    }
+	
+}
